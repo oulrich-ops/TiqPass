8284451f98ec03d35a1aa05ba0f1aedb2c1ff5a5
Kisway <ulrichouedraogo500@gmail.com>
2025-04-17 13:20:00 +0200

    x

---
+++ src/main/java/com/tiqkis/tiqpass/TiqpassApplication.java
@@ -0,0 +1,13 @@
+package com.tiqkis.tiqpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiqpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiqpassApplication.class, args);
+	}
+
+}
